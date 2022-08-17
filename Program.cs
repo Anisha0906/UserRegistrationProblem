ff95caa3f99c83b069f7c852cf066f5691e7e3e6
Anisha0906 <anishadas880@gmail.com>
2022-08-17 21:39:58 +0530

    Add:Valid First Name

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserRegistratiponProblem;
+
+namespace UserRegistrationProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserRegistration validateUser = new UserRegistration();
+            validateUser.validateFirstName("Anisha");
+            Console.ReadKey();
+        }
+    }
+}
